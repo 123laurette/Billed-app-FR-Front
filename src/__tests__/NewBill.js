dbbb7370be2074eceeae3d73ecb6db6f3bab0a9f
laurette <dalmorolaurette@orange.fr>
2022-06-29 20:35:15 +0200

    essai de test NewBill.js

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-
+import "@testing-library/jest-dom"
 import { screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
@@ -9,6 +9,55 @@ import NewBill from "../containers/NewBill.js"
 //******************************************************** */
 //                CONTINUER LE CODE 
 //********************************************************* */
+
+describe("Given I am connected as an employee", () => {
+  describe("je clique sur le bouton nouvelle note de frais", () => {
+    test("un formulaire de saisie apparait", () => {
+
+    })
+  })
+
+
+  describe("je suis sur une nouvelle facture et je clique sur type de dépense", () => { 
+    test("Then une liste déroulante s'ouvre", () => {
+
+
+    })
+  })
+
+
+  describe("je suis sur une nouvelle facture et je clique sur le calendrier", () => {
+    test ("je choisis une date", () => {
+
+    })
+  })
+  
+  describe("je suis sur une nouvelle facture et je ne choisi pas de date", () => {
+    test ("un message apparait", () => {
+
+    })
+  })
+
+  describe("je suis sur une nouvelle facture et je ne rempli pas le champ TTC", () => {
+    test ("un message apparait", () => {
+
+    })
+  })
+
+  describe("je suis sur une nouvelle facture et je rempli le champ TTC avec une virgule", () => {
+    test ("un message apparait", () => {
+
+    })
+  })
+
+
+
+})
+
+
+
+
+
 
 describe("Given I am connected as an employee", () => { //je suis connectée en tant qu'employée
   describe("When I am on NewBill Page", () => { //je suis sur une nouvelle facture

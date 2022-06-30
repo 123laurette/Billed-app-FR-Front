a5594e639ba3cd893a166e6db6a4df71d617467d
laurette <dalmorolaurette@orange.fr>
2022-06-30 18:01:44 +0200

    1er test de newbill réussi

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,9 +2,9 @@
  * @jest-environment jsdom
  */
 import "@testing-library/jest-dom"
-import { screen } from "@testing-library/dom"
+import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
-import Bills from "../containers/Bills.js" //endroit ou se trouve evt sur btn_new-bill
+import BillsUI from "../views/BillsUI.js" //endroit ou se trouve sur btn_new-bill
 import NewBill from "../containers/NewBill.js"
 
 //******************************************************** */
@@ -12,16 +12,21 @@ import NewBill from "../containers/NewBill.js"
 //********************************************************* */
 
 describe("Given I am connected as an employee", () => {
-  describe("je clique sur le bouton nouvelle note de frais", () => {
-    test("un formulaire de saisie apparait", () => {
-      const html = NewBillUI()
+  describe("quand je clique sur le bouton nouvelle note de frais", () => {
+    test("Alors un formulaire de saisie apparait", () => {
+      const html = NewBillUI          //crea const qui represente le formulaire
       document.body.innerHTML = html
+      const btn = BillsUI             //crea const qui represente l'endroit du btn
+      document.body.innerHTML = btn
       
       const btnNewBill = screen.getByTestId("btn-new-bill");  //cible le btn
-      const newBill = screen.getByTestId("root"); //cible la NewBill
-      const ouvreNewBill = jest.fn((e) => e.display = Block); //simule le click et le display block
-      btnNewBill.addEventListener("click", ouvreNewBill);// evt au click sur le btn
-      expect(ouvreNewBill).toBe(newBill); //la simulation doit donner la newbill
+      const ouvreNewBill = jest.fn((e) => e.style.display = Block); //simule la fonction et le display block
+
+      fireEvent.click(btnNewBill);  //simule l'evenement
+
+      btnNewBill.addEventListener("click", ouvreNewBill);// evt au click sur le btn et prise en compte de la fonction
+
+      expect(html).toBeTruthy(); //la simulation doit donner la newbill
 
 
     })

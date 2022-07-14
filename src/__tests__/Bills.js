1559508ce65a8a0ae8f7d0e14e54cc8afec87f06
laurette <dalmorolaurette@orange.fr>
2022-07-14 17:12:18 +0200

    test newbill a 65.12 %

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -7,10 +7,15 @@ import "@testing-library/jest-dom"
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js" //contient la créa dom de la note de frais
 import { bills } from "../fixtures/bills.js"  //contient les données du test
-import { ROUTES_PATH} from "../constants/routes.js";//j'importe la const routes_path
+import Bills from "../containers/Bills.js"
+
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";//j'importe la const routes_path
 import {localStorageMock} from "../__mocks__/localStorage.js";//j'importe la const localstoragemock
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
+jest.mock("../app/Store", () => mockStore)
+
 
 // *********************************test sur l'interface employée des anciennes factures*********************************************
 
@@ -42,4 +47,68 @@ describe("Given I am connected as an employee", () => { //je suis connecté en t
       expect(dates).toEqual(datesSorted)  //je m'attends a ce que les données dates soit égal aux données datesSorted
     })  //la résolution du bug à eu lieu sur le fichier views/billsUI, car c'est le fichier d'affichage des bills(il manquait le trie par ordre croissant)
   })
+
+
+
+
+  /*describe("When I click on the button 'Nouvelle note de frais'", () => {
+    test("Then I should navigate to #employee/bill/new", () => {
+      // Vérifie qu'on arrive bien sur la page NewBill
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const billsPage = new Bills({
+        document, onNavigate, store: null, bills: bills, localStorage: window.localStorage
+      })
+      // on récupère la fonction pour le test
+      const handleClickNewBill = jest.fn(billsPage.handleClickNewBill);
+      // on récupère l'accès au bouton qui dirige vers la page souhaitée
+      const btnNewBill = getByTestId(document.body, "btn-new-bill");
+      // on simule l'action
+      btnNewBill.addEventListener("click", handleClickNewBill);
+      userEvent.click(btnNewBill);
+      // on vérifie que la fonction est appelée et que la page souhaitée s'affiche
+      expect(handleClickNewBill).toHaveBeenCalled();
+      expect(
+        getByText(document.body, "Envoyer une note de frais")
+      ).toBeTruthy();
+    });
+  });
+
+  describe("When I click on the eye icon", () => {
+    test("A modal should open", () => {
+      // Vérifie l'ouverture de la modale si click sur le bouton "oeil" d'une facture
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const billsPage = new Bills({
+        document, onNavigate, store: null, bills: bills, localStorage: window.localStorage
+      })
+      // on affiche les factures dans le HTML
+      document.body.innerHTML = BillsUI({ data: { bills } })
+      // on mock la modale
+      $.fn.modal = jest.fn();
+      // on récupère le premier bouton trouvé
+      const firstEyeIcon = getAllByTestId(document.body, "btn-new-bill")[0];
+      // on récupère la fonction qui ouvre la modale
+      const handleClickIconEye = jest.fn(
+        billsPage.handleClickIconEye(firstEyeIcon)
+      );
+      // on simule l'action
+      firstEyeIcon.addEventListener("click", handleClickIconEye);
+      userEvent.click(firstEyeIcon);
+      // on vérifie l'appel de la fonction et la présence de la modale
+      expect(handleClickIconEye).toHaveBeenCalled();
+      const modal = screen.getByTestId("modale");
+      expect(modal).toBeTruthy();
+    })
+  })*/  
 })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -5,25 +5,26 @@ import "@testing-library/jest-dom"
 import { fireEvent, screen } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
-import BillsUI from "../views/BillsUI.js" //endroit ou se trouve sur btn_new-bill
-import {localStorageMock} from "../__mocks__/localStorage.js";//j'importe la const localstoragemock
+import {localStorageMock} from "../__mocks__/localStorage.js";
 import { ROUTES } from '../constants/routes'
 window.alert = jest.fn()
 
 
 
 describe("Given I am connected as an employee", () => {
+
   describe("je suis sur une nouvelle facture et les champs date, TTC et fichier joint sont vides", () => {
     test ("la note de frais reste à l'écran", () => {
 
       //je suis connecté en tant qu'employée
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock 
+      })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
       }))
       
       //je suis sur une nouvelle note de frais
-      const html = NewBillUI()          //crea const qui represente le formulaire
+      const html = NewBillUI()      //crea const qui represente le formulaire
       document.body.innerHTML = html
       
       //je ne remplis pas les champs date, ttc et fichier joint
@@ -43,94 +44,111 @@ describe("Given I am connected as an employee", () => {
       formNewBill.addEventListener("submit", envoiNewBill)//ecoute d'évènement
       fireEvent.submit(formNewBill)//simulation de l'évènement
       expect(screen.getByTestId("form-new-bill")).toBeTruthy()//après l'évènement le formulaire reste à l'écran
-      
     })
   })
 
-
-  describe("quand je clique sur le bouton nouvelle note de frais", () => {
-    test("Alors un formulaire de saisie apparait", () => {
-
-      const html = NewBillUI          //crea const qui represente le formulaire
-      document.body.innerHTML = html
-      const btn = BillsUI             //crea const qui represente l'endroit du btn
-      document.body.innerHTML = btn
-      
-      const btnNewBill = screen.getByTestId("btn-new-bill");  //cible le btn
-      const ouvreNewBill = jest.fn((e) => e.style.display = Block); //simule la fonction et le display block
-
-      fireEvent.click(btnNewBill);  //simule l'evenement
-
-      btnNewBill.addEventListener("click", ouvreNewBill);// evt au click sur le btn et prise en compte de la fonction
-
-      expect(html).toBeTruthy(); //la simulation doit donner la newbill
-    })
-  })
-
-
-  
-  describe("je clique sur choisir un fichier et je telecharge le fichier dans le format jpg", () => {
+  describe("je telecharge le fichier dans le bon format", () => {
     test ("mon champ est validé", () => {
-       //je suis connecté en tant qu'employée
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      //je suis connecté en tant qu'employée
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock 
+      })
       window.localStorage.setItem('user', JSON.stringify({
       type: 'Employee'
       }))
     
-       //je suis sur une nouvelle note de frais
-      const html = NewBillUI()          //crea const qui represente le formulaire
+      //je suis sur une nouvelle note de frais
+        //j'integre le formulaire et le chemin d'accès
+      const html = NewBillUI()         
       document.body.innerHTML = html
-      const onNavigate = (pathname) => {  //je charge le fichier route
-      document.body.innerHTML = ROUTES({ pathname})
+      const onNavigate = (pathname) => {  
+        document.body.innerHTML = ROUTES({ pathname})
       }
-       const newBill = new NewBill({ //je crée une nouvelle instance newbill
+      const newBill = new NewBill({ //je crée une nouvelle instance newbill
         document,
         onNavigate,
         store: null,
         localStorage: window, localStorage,
       })
-      const chargeFichier = jest.fn(newBill => newBill)
-      const fichier = screen.getByTestId("file")
-      const testFormat = new File(["c'est un test"], {
+      //créa const pour fonction qui appel la fonction a tester
+      const chargeFichier = jest.fn((e) => newBill.handleChangeFile(e))
+      
+      const fichier = screen.getByTestId("file")//cible le champ fichier
+      const testFormat = new File(["c'est un test"], {//condition du test
         accept: "image/jpg"
       })
-      fichier.addEventListener("change", chargeFichier)
-      fireEvent.change(fichier, {target: {files: [testFormat]}})
+      fichier.addEventListener("change", chargeFichier)//écoute évènement
+      fireEvent.change(fichier, {target: {files: [testFormat]}})//évènement au change en relation avec la condition du test
       
-      expect(chargeFichier).toHaveBeenCalled()
-      expect(fichier.files[0]).toStrictEqual(testFormat)
+      expect(chargeFichier).toHaveBeenCalled()//je vérifie que le fichier est bien chargé
+      expect(fichier.files[0]).toStrictEqual(testFormat)//je vérifie que le fichier téléchargé est bien conforme à la condition du test
     })
   })
-  describe("je clique sur choisir un fichier et je telecharge le fichier dans un mauvais format", () => {
+
+  describe("je telecharge le fichier dans un mauvais format", () => {
     test ("je reste sur mon formulaire et un message apparait", () => {
        //je suis connecté en tant qu'employée
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock 
+      })
       window.localStorage.setItem('user', JSON.stringify({
       type: 'Employee'
       }))
     
-       //je suis sur une nouvelle note de frais
-      const html = NewBillUI()          //crea const qui represente le formulaire
+      //je suis sur une nouvelle note de frais
+        //j'integre le formulaire et le chemin d'accès
+      const html = NewBillUI()          
       document.body.innerHTML = html
-      const onNavigate = (pathname) => {  //je charge le fichier route
-      document.body.innerHTML = ROUTES({ pathname})
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname})
       }
-       const newBill = new NewBill({ //je crée une nouvelle instance newbill
+      const newBill = new NewBill({ //je crée une nouvelle instance newbill
         document,
         onNavigate,
         store: null,
         localStorage: window, localStorage,
       })
-      const chargeFichier = jest.fn(newBill => newBill)
-      const fichier = screen.getByTestId("file")
-      const testFormat = new File(["c'est un test"], {
+      //créa const pour fonction qui appel la fonction a tester
+      const chargeFichier = jest.fn((e) => newBill.handleChangeFile(e))
+      const fichier = screen.getByTestId("file")//cible le champ fichier
+      const testFormat = new File(["c'est un test"], {//condition du test
       type: "document.txt"
       })
-      fichier.addEventListener("change", chargeFichier)
-      fireEvent.change(fichier, {target: {files: [testFormat]}})
+      fichier.addEventListener("change", chargeFichier)//écoute évènement
+      fireEvent.change(fichier, {target: {files: [testFormat]}})//évènement au change en relation avec la condition du test
       
-      expect(chargeFichier).toHaveBeenCalled()
-      expect(window.alert).toBeTruthy()
+      expect(chargeFichier).toHaveBeenCalled()//je vérifie que le fichier est bien chargé
+      expect(window.alert).toBeTruthy()//je m'attends a recevoir un message
     })
-})
+  })
+  describe("j'ai rempli le formulaire et je le valide avec le bouton envoyer", () => {
+    test("le formulaire doit être envoyé vers la page des factures",  () => {
+     //je suis connecté en tant qu'employée
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock 
+      })
+      window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'
+      }))
+
+      //je suis sur une nouvelle note de frais
+      //j'integre le formulaire et le chemin d'accès
+      const html = NewBillUI()
+      document.body.innerHTML = html
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const newBillEnCours = new NewBill({  //créa instance de NewBill
+        document,
+        onNavigate,
+        store: null,
+        localStorage: window.localStorage,
+      })
+      const formNewBill = screen.getByTestId('form-new-bill')//cible le formulaire
+      expect(formNewBill).toBeTruthy()
+
+      const envoiNewBill = jest.fn((e) => newBillEnCours.handleSubmit(e))//simule la fonction
+      formNewBill.addEventListener('submit', envoiNewBill)//évènement au submit
+      fireEvent.submit(formNewBill)//simule l'évènement
+      expect(envoiNewBill).toHaveBeenCalled()
+      expect(screen.getByText('Mes notes de frais')).toBeTruthy()
+    })
+  })
 })
